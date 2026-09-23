3d95de8385c4a99fc8793927e73a7256e4da06fc
IrishMan05 <grigodssf65@gmail.com>
2026-09-23 12:22:34 +0300

    Update Request DTO for updating events

--- src/main/java/ifmo/poster/monolith/dto/request/event/UpdateEventRequest.java
+++ src/main/java/ifmo/poster/monolith/dto/request/event/UpdateEventRequest.java
@@ -1,5 +1,26 @@
 package ifmo.poster.monolith.dto.request.event;
 
+import java.time.LocalDateTime;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class UpdateEventRequest {
-  
+    @NotBlank
+    @Size(min = 5, max = 100)
+    private String eventName;
+
+    @NotBlank
+    @Size(min = 50, max = 200)
+    private String description;
+
+    @NotBlank
+    @Size(min = 5, max = 200)
+    private String location;
+    
+    private LocalDateTime dateTime; 
 }
